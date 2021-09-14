3779a25743e1c1b1e88af65053aa61f075660c17
Tu Hoang <rebyn@me.com>
2021-09-14 14:10:56 +0000

    21:10

--- chapter3/main.tf
+++ chapter3/main.tf
@@ -1,41 +1,70 @@
 provider "aws" {
-    region = "ap-southeast-1"
+  region = "ap-southeast-1"
 }
 
 variable "server_port" {
-    description = "The port the server will use for HTTP requests"
-    type = number
-    default = 8080
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
 }
 
 resource "aws_instance" "example" {
-    ami = "ami-0c55b159cbfafe1f0"
-    instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
-    user_data = <<-EOF
+  user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World (TuHoang Gitpod) > index.hmtl
                 nohup busybox httpd -f -p ${var.server_port} &
                 EOF
 
-    tags = {
-        Name = "terraform-example"
-    }
+  tags = {
+    Name = "terraform-example"
+  }
 }
 
 resource "aws_security_group" "instance" {
-    name = "terraform-example-instance"
+  name = "terraform-example-instance"
 
-    ingress {
-        from_port = var.server_port
-        to_port = var.server_port
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0c55b159cbfafe1f0"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World (TuHoang Gitpod) > index.hmtl
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
 }
 
 output "public_ip" {
-    value = aws_instance.example.public_ip
-    description = "The public IP address of the web server"
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
