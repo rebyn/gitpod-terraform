39f9fba7748c2247ff8715b059b252edc7d7ecb0
Tu Hoang <rebyn@me.com>
2021-09-14 13:48:26 +0000

    20:48

--- chapter3/main.tf
+++ chapter3/main.tf
@@ -2,10 +2,40 @@ provider "aws" {
     region = "ap-southeast-1"
 }
 
+variable "server_port" {
+    description = "The port the server will use for HTTP requests"
+    type = number
+    default = 8080
+}
+
 resource "aws_instance" "example" {
     ami = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World (TuHoang Gitpod) > index.hmtl
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+
     tags = {
         Name = "terraform-example"
     }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = var.server_port
+        to_port = var.server_port
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+output "public_ip" {
+    value = aws_instance.example.public_ip
+    description = "The public IP address of the web server"
 }
